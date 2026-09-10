6fa1df469db6f1de2e24e61524b6415ae05aec65
Archari2517 <Archari.k@ku.th>
2026-09-10 20:59:25 +0700

    Edit noti

--- api/send-notification.js
+++ api/send-notification.js
@@ -16,10 +16,10 @@
 //   FIREBASE_PRIVATE_KEY
 
 import admin from 'firebase-admin';
-
+import { cert } from 'firebase-admin/app';
 if (!admin.apps?.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
+    credential: cert({ // 2. เปลี่ยนตรงนี้เป็น cert(...) โดยไม่ต้องมี admin.credential
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
